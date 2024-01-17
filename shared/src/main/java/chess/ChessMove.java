418b457042cf48e8a42d1cc3d7ad98fe9313f802
Max Whitney <maxman010101@gmail.com>
2024-01-17 15:18:09 -0700

    added to string, hash, and overidden equals methods to chess  ove and position classes

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -10,6 +12,19 @@ public class ChessMove {
     public ChessPosition startPosition;
     public ChessPosition endPosition;
     public ChessPiece.PieceType promotionPiece;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.equals(startPosition, chessMove.startPosition) && Objects.equals(endPosition, chessMove.endPosition) && promotionPiece == chessMove.promotionPiece;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPosition, endPosition, promotionPiece);
+    }
 
     @Override
     public String toString() {
