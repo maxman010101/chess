e921722b9ffae3bbfd630616c806f0c67f0d722d
Max Whitney <maxman010101@gmail.com>
2024-03-13 13:41:22 -0600

    got it to print out a board based on tic tac toe code, now editing code to make it a new chess board setup

--- server/src/main/java/ui/ChessBoardUI.java
+++ server/src/main/java/ui/ChessBoardUI.java
@@ -1,7 +1,11 @@
 package ui;
 
+import chess.ChessBoard;
+
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
 import static ui.EscapeSequences.*;
 
 public class ChessBoardUI {
@@ -15,6 +19,7 @@ public class ChessBoardUI {
     private static final String N = " N ";
     private static final String B = " B ";
     private static final String P = " P ";
+    private ChessBoard gameBoard = new ChessBoard();
     public static void main(String[] args) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
@@ -22,16 +27,15 @@ public class ChessBoardUI {
 
         drawHeaders(out);
 
-        //drawChessBoard(out);
+        drawChessBoard(out);
 
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_WHITE);
     }
     private static void drawHeaders(PrintStream out) {
-
         setBlack(out);
 
-        String[] headers = { "a", "b", "c", "d", "e", "f", "g", "h" };
+        String[] headers = { " a ", " b ", " c ", " d ", " e ", " f ", " g ", " h " };
         for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
             drawHeader(out, headers[boardCol]);
 
@@ -61,5 +65,83 @@ public class ChessBoardUI {
     private static void setBlack(PrintStream out) {
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_BLACK);
+    }
+    private static void drawChessBoard(PrintStream out) {
+
+        for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
+
+            drawRowOfSquares(out);
+
+            if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
+                drawVerticalLine(out);
+                setBlack(out);
+            }
+        }
+    }
+    private static void drawRowOfSquares(PrintStream out) {
+
+        for (int squareRow = 0; squareRow < SQUARE_SIZE_IN_CHARS; ++squareRow) {
+            for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+                setWhite(out);
+
+                if (squareRow == SQUARE_SIZE_IN_CHARS / 2) {
+                    int prefixLength = SQUARE_SIZE_IN_CHARS / 2;
+                    int suffixLength = SQUARE_SIZE_IN_CHARS - prefixLength - 1;
+
+                    out.print(EMPTY.repeat(prefixLength));
+                    printWhitePiece(out, K);
+                    printBlackPiece(out, K);
+                    out.print(EMPTY.repeat(suffixLength));
+                }
+                else {
+                    out.print(EMPTY.repeat(SQUARE_SIZE_IN_CHARS));
+                }
+
+                if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
+                    // Draw right line
+                    setRed(out);
+                    out.print(EMPTY.repeat(LINE_WIDTH_IN_CHARS));
+                }
+
+                setBlack(out);
+            }
+
+            out.println();
+        }
+    }
+    private static void drawVerticalLine(PrintStream out) {
+
+        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_CHARS +
+                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_CHARS;
+
+        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_CHARS; ++lineRow) {
+            setRed(out);
+            out.print(EMPTY.repeat(boardSizeInSpaces));
+
+            setBlack(out);
+            out.println();
+        }
+    }
+    private static void setWhite(PrintStream out) {
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+    private static void setRed(PrintStream out) {
+        out.print(SET_BG_COLOR_RED);
+        out.print(SET_TEXT_COLOR_RED);
+    }
+    private static void printWhitePiece(PrintStream out, String piece) {
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_BLUE);
+
+        out.print(piece);
+
+        setWhite(out);
+    }
+    private static void printBlackPiece(PrintStream out, String piece) {
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_RED);
+        out.print(piece);
+        setWhite(out);
     }
 }
