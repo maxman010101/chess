a6ff5665e3eff5f7befe654d1fe5ce45646b61f1
Max Whitney <maxman010101@gmail.com>
2024-03-20 21:56:36 -0600

    made my facade unit tests

--- client/test/java/clientTests/ServerFacadeTests.java
+++ client/test/java/clientTests/ServerFacadeTests.java
@@ -1,6 +1,9 @@
 package clientTests;
 
+import chess.ChessGame;
 import dataAccess.DataAccessException;
+import dataAccess.SQLAuthDataAccess;
+import dataAccess.SQLGameDataAccess;
 import dataAccess.SQLUserDataAccess;
 import org.junit.jupiter.api.*;
 import server.Server;
@@ -9,6 +12,7 @@ import ui.ChessServerFacade;
 import ui.ResponseException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static ui.State.SIGNEDOUT;
 
 
 public class ServerFacadeTests {
@@ -47,5 +51,101 @@ public class ServerFacadeTests {
         SQLUserDataAccess userDao = new SQLUserDataAccess();
         Assertions.assertNotNull(userDao.getUser("a"));
     }
-    
+    @Test
+    public void goodLogin() throws ResponseException {
+        facade.register("player1", "password", "p1@email.com");
+        facade.logOut();
+        var authData = facade.login("player1", "password");
+        assertTrue(authData.authToken.length() > 10);
+    }
+    @Test
+    public void badLogin() throws ResponseException, responses.ResponseException, DataAccessException {
+        //cannot log in if the user isn't registered
+        facade.register("a", "b", "c");
+        facade.logOut();
+        SQLUserDataAccess userDao = new SQLUserDataAccess();
+        Assertions.assertNotNull(userDao.getUser("a"));
+        Assertions.assertNull(userDao.getUser("otherName"));
+    }
+    @Test
+    public void goodLogOut() throws ResponseException, responses.ResponseException, DataAccessException {
+        var token = facade.register("a", "b", "c");
+        facade.logOut();
+        SQLAuthDataAccess authDao = new SQLAuthDataAccess();
+        assertNull(authDao.getAuth(token.authToken));
+    }
+    @Test
+    public void badLogOut() {
+        //logging out without logging in or registering
+        boolean thrown = false;
+        try {
+            facade.logOut();
+        } catch (ResponseException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+    @Test
+    public void madeGoodGame() throws ResponseException {
+        facade.register("a", "b", "c");
+        var game = facade.createGame("game1");
+        assertEquals(1, game.gameID());
+    }
+    @Test
+    public void gameCreationFailed() throws ResponseException {
+        //trying to create an unnamed game
+        facade.register("a", "b", "c");
+        boolean thrown = false;
+        try {
+            facade.createGame(null);
+        } catch (ResponseException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+    @Test
+    public void gameListed() throws ResponseException {
+        //game created is listed
+        facade.register("a", "b", "c");
+        facade.createGame("game1");
+        assertNotNull(facade.listGames().games().getFirst());
+    }
+    @Test
+    public void gamesNotListed() throws ResponseException {
+        //trying to list nobody's games/logged out when trying to make the command
+        facade.register("a", "b", "c");
+        facade.logOut();
+        boolean thrown = false;
+        try {
+            facade.listGames();
+        } catch (ResponseException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+    @Test
+    public void joinedAGame() throws ResponseException {
+        facade.register("a", "b", "c");
+        var game = facade.createGame("game1");
+        boolean thrown = false;
+        try {
+            facade.joinGame(game.gameID(), ChessGame.TeamColor.WHITE, facade.listGames().games().getFirst(), null);
+        } catch (ResponseException e) {
+            thrown = true;
+        }
+        assertFalse(thrown);
+    }
+    @Test
+    public void tryToJoinFakeGame() throws ResponseException {
+        facade.register("a", "b", "c");
+        facade.createGame("game1");
+        boolean thrown = false;
+        try {
+            facade.joinGame(2, ChessGame.TeamColor.WHITE, facade.listGames().games().getFirst(), null);
+
+        } catch (ResponseException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
 }
